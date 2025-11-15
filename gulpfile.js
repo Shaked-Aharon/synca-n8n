833a32cad23fe5a108cae35837d48d9180d263a6
Shaked Aharon <srek123@gmail.com>
2025-11-15 19:36:09 +0200

    Remove commented lines and set build to copy to custom-node and added information to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const { task, src, dest } = require('gulp');
+// const { Transform } =  require('stream');
 
 task('build:icons', copyIcons);
+task('build:move', moveBuild)
 
 function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
@@ -14,3 +16,59 @@ function copyIcons() {
 
 	return src(credSource).pipe(dest(credDestination));
 }
+
+function moveBuild() {
+	const distSource = path.resolve('dist', '**', "*");
+	const n8nCustomNodesDestination = path.resolve('C:\\Users\\shaked\\Desktop\\DevOps\\Scripts\\n8n\\custom-nodes\\n8n-nodes-synca');
+
+	return src(distSource).pipe(dest(n8nCustomNodesDestination));
+}
+
+// function filterNonEmptyFiles() {
+// 	return new Transform({
+// 		objectMode: true,
+// 		transform(file, _enc, cb) {
+// 			// If no contents at all – skip
+// 			if (file.isNull()) {
+// 				return cb();
+// 			}
+
+// 			// Most common case: buffered files
+// 			if (file.isBuffer()) {
+// 				const text = file.contents.toString();
+// 				if (text.trim().length === 0) {
+// 					// empty or whitespace-only -> skip
+// 					return cb();
+// 				}
+// 				// non-empty -> pass through
+// 				this.push(file);
+// 				return cb();
+// 			}
+
+// 			// If you don't use streaming files, you can just skip this branch:
+// 			if (file.isStream()) {
+// 				let data = '';
+// 				file.contents.on('data', (chunk) => {
+// 					data += chunk.toString();
+// 				});
+// 				file.contents.on('end', () => {
+// 					if (data.trim().length > 0) {
+// 						this.push(file);
+// 					}
+// 					cb();
+// 				});
+// 			}
+// 		},
+// 	});
+// }
+
+// export function moveBuild() {
+// 	const distSource = path.resolve('dist', '**');
+// 	const n8nCustomNodesDestination = path.resolve(
+// 		'C:\\Users\\shaked\\Desktop\\DevOps\\Scripts\\n8n\\custom-nodes\\n8n-nodes-synca',
+// 	);
+
+// 	return src(distSource, { nodir: true })
+// 		.pipe(filterNonEmptyFiles())
+// 		.pipe(dest(n8nCustomNodesDestination));
+// }
--- nodes/Priority/SyncaPriority.node.ts
+++ nodes/Priority/SyncaPriority.node.ts
@@ -160,207 +160,6 @@ export class SyncaPriority implements INodeType {
 		],
 	};
 
-	// 	loadOptions: {
-	// 		async getCredentials(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-	// 			try {
-	// 				const credentials = await this.getCredentials<{ apiToken: string; baseUrl: string; }>('customSyncaApiCredentials');
-	// 				const options: IHttpRequestOptions = {
-	// 					method: 'GET',
-	// 					url: `${credentials.baseUrl}/v1/invoke/get-credentials`,
-	// 					headers: {
-	// 						'x-api-token': credentials?.apiToken as string,
-	// 					},
-	// 				};
-
-	// 				const response = await this.helpers.httpRequest(options);
-
-	// 				if (Array.isArray(response)) {
-	// 					return response.map((cred: any) => ({
-	// 						name: cred.name || cred.id,
-	// 						value: cred.id,
-	// 					}));
-	// 				}
-
-	// 				return [];
-	// 			} catch (error) {
-	// 				return [
-	// 					{
-	// 						name: 'Default Credentials',
-	// 						value: 'default',
-	// 					},
-	// 				];
-	// 			}
-	// 		},
-
-	// 		async getSubforms(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-	// 			try {
-	// 				const credentials = await this.getCredentials<{ apiToken: string; baseUrl: string; }>('customSyncaApiCredentials');
-	// 				const credentialsId = this.getNodeParameter('credentials') as string;
-	// 				const resource = this.getNodeParameter('resource') as string;
-	// 				const resourceToFormName: Record<string, string> = {
-	// 					'products': 'LOGPART',
-	// 				}
-	// 				const url = `${credentials.baseUrl}/v1/invoke/${credentialsId}/get_subforms`;
-	// 				const options: IHttpRequestOptions = {
-	// 					method: 'POST',
-	// 					url: url,
-	// 					headers: {
-	// 						'x-api-token': credentials?.apiToken as string,
-	// 					},
-	// 					body: { formName: resourceToFormName[resource] }
-	// 				};
-
-	// 				const response = await this.helpers.httpRequest(options);
-	// 				if (Array.isArray(response.data)) {
-	// 					return response.data.map((cred: any) => ({
-	// 						name: cred.name || cred.id,
-	// 						value: cred.id,
-	// 					}));
-	// 				}
-
-	// 				return [];
-	// 			} catch (error) {
-	// 				return [
-	// 					{
-	// 						name: 'Default Credentials',
-	// 						value: 'default',
-	// 					},
-	// 				];
-	// 			}
-	// 		},
-
-	// 		async getForms(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-	// 			try {
-	// 				const credentials = await this.getCredentials<{ apiToken: string; baseUrl: string; }>('customSyncaApiCredentials');
-	// 				const credentialsId = this.getNodeParameter('credentials') as string;
-
-	// 				const url = `${credentials.baseUrl}/v1/invoke/${credentialsId}/get_forms`;
-	// 				const options: IHttpRequestOptions = {
-	// 					method: 'POST',
-	// 					url: url,
-	// 					headers: {
-	// 						'x-api-token': credentials?.apiToken as string,
-	// 					}
-	// 				};
-
-	// 				const response = await this.helpers.httpRequest(options);
-	// 				if (Array.isArray(response.data)) {
-	// 					return response.data.map((cred: any) => ({
-	// 						name: cred.name || cred.id,
-	// 						value: cred.id,
-	// 					}));
-	// 				}
-
-	// 				return [];
-	// 			} catch (error) {
-	// 				return [
-	// 					{
-	// 						name: 'Default Credentials',
-	// 						value: 'default',
-	// 					},
-	// 				];
-	// 			}
-
-	// 		},
-	// 		async getFormFields(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-	// 			try {
-	// 				const credentials = await this.getCredentials<{ apiToken: string; baseUrl: string; }>('customSyncaApiCredentials');
-	// 				const credentialsId = this.getNodeParameter('credentials') as string;
-	// 				const formName = this.getNodeParameter('formName') as string;
-
-	// 				const url = `${credentials.baseUrl}/v1/invoke/${credentialsId}/get_form_fields`;
-	// 				const options: IHttpRequestOptions = {
-	// 					method: 'POST',
-	// 					url: url,
-	// 					headers: {
-	// 						'x-api-token': credentials?.apiToken as string,
-	// 					},
-	// 					body: { formName }
-	// 				};
-
-	// 				const response = await this.helpers.httpRequest(options);
-	// 				if (Array.isArray(response.data)) {
-	// 					return response.data.map((cred: any) => ({
-	// 						name: cred.name || cred.id,
-	// 						value: cred.id,
-	// 					}));
-	// 				}
-
-	// 				return [];
-	// 			} catch (error) {
-	// 				return [
-	// 					{
-	// 						name: 'Default Credentials',
-	// 						value: 'default',
-	// 					},
-	// 				];
-	// 			}
-	// 		},
-
-	// 		async getProcedures(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-	// 			try {
-	// 				const credentials = await this.getCredentials<{ apiToken: string; baseUrl: string; }>('customSyncaApiCredentials');
-	// 				const credentialsId = this.getNodeParameter('credentials') as string;
-
-	// 				const url = `${credentials.baseUrl}/v1/invoke/${credentialsId}/get_procedures`;
-	// 				const options: IHttpRequestOptions = {
-	// 					method: 'POST',
-	// 					url: url,
-	// 					headers: {
-	// 						'x-api-token': credentials?.apiToken as string,
-	// 					},
-	// 				};
-
-	// 				const response = await this.helpers.httpRequest(options);
-	// 				if (Array.isArray(response.data)) {
-	// 					return response.data.map((cred: any) => ({
-	// 						name: cred.name || cred.id,
-	// 						value: cred.id,
-	// 					}));
-	// 				}
-
-	// 				return [];
-	// 			} catch (error) {
-	// 				return [];
-	// 			}
-	// 		},
-	// 	},
-	// 	resourceMapping: {
-	// 		async getMappingColumns(this: ILoadOptionsFunctions): Promise<ResourceMapperFields> {
-	// 			try {
-	// 				const credentials = await this.getCredentials<{ apiToken: string; baseUrl: string; }>('customSyncaApiCredentials');
-	// 				const credentialsId = this.getNodeParameter('credentials') as string;
-
-	// 				const url = `${credentials.baseUrl}/v1/invoke/${credentialsId}/get_procedure_columns`;
-	// 				const options: IHttpRequestOptions = {
-	// 					method: 'POST',
-	// 					url: url,
-	// 					headers: {
-	// 						'x-api-token': credentials?.apiToken as string,
-	// 					},
-	// 					body: { procedure_name: this.getNodeParameter('procedureName') }
-	// 				};
-
-	// 				const response = await this.helpers.httpRequest(options);
-
-	// 				let results: ResourceMapperField[] = [];
-
-	// 				if (Array.isArray(response.data.EditFields)) {
-	// 					results = response.data.EditFields.map((f: any, i: number) => ({
-	// 						id: `${f.field}`,
-	// 						required: f.mandatory === 1,
-	// 						displayName: f.title,
-	// 						type: 'string',
-	// 						display: true,
-	// 					}) as ResourceMapperField);
-	// 				}
-	// 				return { fields: results };
-	// 			} catch (error) {
-	// 				return { fields: [] };
-	// 			}
-	// 		},
-	// 	}
-	// };
 	methods = PriorityMethods;
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
@@ -380,59 +179,6 @@ export class SyncaPriority implements INodeType {
 				let requestParams: any = {};
 				let tempFields: ResourceMapperValue;
 				switch (operation) {
-					// case 'create_product':
-					// case 'update_product':
-					// 	requestParams = {
-					// 		formName: operationToFormName[operation],
-					// 		fields: (this.getNodeParameter('rowData', 0, {}) as any).value
-					// 	};
-					// 	operation = 'update_row_in_form';
-					// 	break;
-					// case 'get_product':
-					// 	requestParams = {
-					// 		id: this.getNodeParameter('productId', i),
-					// 		include_subforms: this.getNodeParameter('includeSubforms', i, []),
-					// 	};
-					// 	break;
-					// case 'create_customer':
-					// case 'update_customer':
-					// 	requestParams = {
-					// 		formName: operationToFormName[operation],
-					// 		fields: (this.getNodeParameter('rowData', 0, {}) as any).value
-					// 	};
-					// 	operation = 'update_row_in_form';
-					// 	break;
-					// case 'get_customer':
-					// 	requestParams = {
-					// 		id: this.getNodeParameter('customerId', i),
-					// 		include_subforms: this.getNodeParameter('includeSubforms', i, []),
-					// 	};
-					// 	break;
-					// case 'create_order':
-					// case 'update_order':
-					// 	requestParams = {
-					// 		formName: operationToFormName[operation],
-					// 		fields: (this.getNodeParameter('rowData', 0, {}) as any).value
-					// 	};
-					// 	break;
-					// case 'get_order':
-					// 	requestParams = {
-					// 		id: this.getNodeParameter('orderId', i),
-					// 		include_subforms: this.getNodeParameter('includeSubforms', i, []),
-					// 	};
-					// 	break;
-					// case 'create_invoice':
-					// case 'update_invoice':
-					// 	requestParams = {
-					// 		formName: operationToFormName[operation],
-					// 		fields: (this.getNodeParameter('rowData', 0, {}) as any).value
-					// 	};
-					// 	break;
-					// case 'get_invoice':
-					// 	requestParams = {
-					// 		id: this.getNodeParameter('invoiceId', i),
-					// 	};
-					// 	break;
 					case 'get_product':
 					case 'get_customer':
 					case 'get_order':
@@ -444,27 +190,8 @@ export class SyncaPriority implements INodeType {
 							include_subforms: this.getNodeParameter('includeSubforms', i, []),
 							formName: operationToFormName[operation]
 						};
-						// if(operation === 'get_purchase_order') {operation = 'get_row_in_form'}
 						operation = 'get_row_in_form'
 						break;
-					// case 'create_product':
-					// case 'update_product':
-					// case 'create_customer':
-					// case 'update_customer':
-					// case 'create_order':
-					// case 'update_order':
-					// case 'create_invoice':
-					// case 'update_invoice':
-					// case 'create_purchase_order':
-					// case 'update_purchase_order':
-					// 	tempFields = this.getNodeParameter('rowData', 0, {}) as any;
-					// 	requestParams = {
-					// 		formName: operationToFormName[operation],
-					// 		fields: processFieldsToSend(tempFields.value, tempFields.schema)
-					// 	};
-					// 	if (operation.startsWith('update_')) operation = 'update_row_in_form';
-					// 	else operation = 'add_to_form';
-					// 	break;
 					case 'create_product':
 					case 'update_product':
 					case 'create_customer':
@@ -499,28 +226,13 @@ export class SyncaPriority implements INodeType {
 							);
 						}
 						break;
-					// case 'get_purchase_order':
-					// 	requestParams = {
-					// 		id: this.getNodeParameter('purchaseOrderId', i),
-					// 	};
-					// 	break;
 					case 'get_agent':
 						requestParams = {
 							id: this.getNodeParameter('agentId', i),
 						};
 						break;
 					case 'run_procedures':
-						// const procedureParams = this.getNodeParameter('fields', i) as any;
 						const parameters = (this.getNodeParameter('fields', 0, {}) as any);
-						console.log({ parameters })
-						// const parameters: any = {};
-
-						// if (procedureParams && procedureParams.parameter) {
-						// 	for (const param of procedureParams.parameter) {
-						// 		parameters[param.name] = param.value;
-						// 	}
-						// }
-
 						requestParams = {
 							procedure_name: this.getNodeParameter('procedureName', i),
 							parameters: parameters.value//: Object.keys(parameters).length > 0 ? parameters : undefined,
@@ -568,12 +280,6 @@ export class SyncaPriority implements INodeType {
 							filters: searchSubFormFilters.map(f => getFinalFilter(f))
 						};
 						break;
-					// case 'add_to_form':
-					// 	requestParams = {
-					// 		formName: this.getNodeParameter('formName', i),
-					// 		fields: (this.getNodeParameter('rowData', 0, {}) as any).value,
-					// 	};
-					// 	break;
 					case 'add_to_sub_form':
 						tempFields = this.getNodeParameter('rowData', 0, {}) as any;
 						requestParams = {
