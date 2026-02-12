03a86a7253b037fe5cbe18feec2cd1f4af3a8176
Shaked Aharon <srek123@gmail.com>
2026-02-12 16:38:12 +0200

    [VERSION 1.0.35] SyncaService wont add to signature empty objects for body

--- dist/nodes/shared/SyncaService.js
+++ dist/nodes/shared/SyncaService.js
@@ -17,7 +17,7 @@ class SyncaService {
         const url = `${baseUrl}${path}`;
         const timestamp = Math.floor(Date.now() / 1000).toString();
         let bodyForSignature = body;
-        if (typeof body === 'object') {
+        if (typeof body === 'object' && Object.keys(body).length > 0) {
             bodyForSignature = JSON.stringify(body);
         }
         else if (body === undefined || body === null) {
